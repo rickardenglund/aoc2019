dc892aa5f664a01959023cfe878589955da5f160
Rickard Englund <rickard.englund@gmail.com>
2019-12-01 08:17:05 +0100

    update read file

--- inputs/input.go
+++ inputs/input.go
@@ -4,11 +4,17 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func GetLines(path string) []string {
 	file, err := os.Open(path)
 	if err != nil {
+		abspath, absErr := filepath.Abs(path)
+		if absErr != nil {
+			log.Fatal("Failed to get absolute path from: %v\n", path)
+		}
+		log.Printf("absolute path: %v\n", abspath)
 		log.Fatal(err)
 	}
 	defer file.Close()
