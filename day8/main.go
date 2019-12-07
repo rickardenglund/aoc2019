615308dec3f8abec23bf66da60d8a379c77fe3c3
Rickard Englund <rickard.englund@gmail.com>
2019-12-07 17:57:48 +0100

    template d8

---
+++ day8/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	p1 := part1()
+	fmt.Printf("part1: %v in %v\n", p1, time.Since(start))
+	start2 := time.Now()
+	p2 := part2()
+	fmt.Printf("part2: %v in %v\n", p2, time.Since(start2))
+}
+
+func part1() int {
+	return -1
+
+}
+
+func part2() int {
+	return -1
+}
